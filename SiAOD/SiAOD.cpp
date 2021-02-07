75537176c8ff40568be6df3318c0459ebb773e97
Vitaly Suslov <arthas.menethil2002@bk.ru>
2021-02-07 12:25:20 +0700

    Добавьте файлы проекта.

---
+++ SiAOD/SiAOD.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <time.h>
+using namespace std;
+
+const int N = 10;
+int A[N];
+
+void FillInc() {
+    for (int i = 0; i < N; i++)
+        A[i] = i + 1;
+};
+
+void FillDec() {
+    int k = N;
+    for (int i = 0; i < N; i++) {
+        A[i] = k;
+        k--;
+    }
+};
+
+void FillRand() {
+    srand(time(NULL));
+    for (int i = 0; i < N; i++)
+        A[i] = rand() % 10 + 1;
+};
+
+void PrintMas() {
+    for (int i = 0; i < N; i++)
+        cout << A[i] << " ";
+    cout << endl;
+};
+
+int CheckSum() {
+    int z = 0;
+    for (int i = 0; i < N; i++)
+        z += A[i];
+    return z;
+};
+
+int RunNumber() {
+    int count = 0;
+    for (int i = 0; i < N-1; i++) {
+        if (A[i] > A[i + 1]) {
+            count += 1;
+        }
+    }
+    count += 1;
+    return count;
+};
+
+void SelectSort(int Massive[], int size) {
+    int Moves = 0, Compares = 0;
+    for (int i = 0; i < N-1; i++) {
+        int min = i;
+        for (int j = i + 1; j < N ; j++) {
+            if (RunNumber() != 1) {
+                Compares++;
+                if (Massive[j] < Massive[min]) {
+                    min = j;
+                }
+            }
+        }
+        if (Massive[i] != Massive[min]) {
+            swap(Massive[i], Massive[min]);
+            Moves++;
+        }
+    }
+    cout << "\nM = " << Moves << " C = " << Compares << endl;
+};
+
+int main() {
+    cout << "Random massive\n";
+    FillRand();
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    SelectSort(A, N);
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    cout << "Theoretical: M = " << 3*(N - 1) << " C = " << (N*N - N)/2;
+
+    cout << "\nInc massive\n";
+    FillInc();
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    SelectSort(A, N);
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
+
+    cout << "\nDec massive\n";
+    FillDec();
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    SelectSort(A, N);
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
+}
+

f6cd6ff1a16bd238465fcfa6d4bb7bbcd0519e28
Vitaly Suslov <arthas.menethil2002@bk.ru>
2021-02-12 11:35:20 +0700

    Доделал

--- SiAOD/SiAOD.cpp
+++ SiAOD/SiAOD.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 using namespace std;
 
-const int N = 10;
+const int N = 20;
 int A[N];
 
 void FillInc() {
@@ -53,25 +53,25 @@ int RunNumber() {
 void SelectSort(int Massive[], int size) {
     int Moves = 0, Compares = 0;
     for (int i = 0; i < N-1; i++) {
-        int min = i;
-        for (int j = i + 1; j < N ; j++) {
-            if (RunNumber() != 1) {
+        int min = i; 
+        if (RunNumber() != 1) {
+            for (int j = i + 1; j < N; j++) {
                 Compares++;
                 if (Massive[j] < Massive[min]) {
                     min = j;
                 }
             }
-        }
-        if (Massive[i] != Massive[min]) {
-            swap(Massive[i], Massive[min]);
-            Moves++;
+            if ((i != min) && (min > i)) {
+                swap(Massive[i], Massive[min]);
+                Moves += 3;
+            }
         }
     }
     cout << "\nM = " << Moves << " C = " << Compares << endl;
 };
 
 int main() {
-    cout << "Random massive\n";
+    cout << "RANDOM MASSIVE\n";
     FillRand();
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
@@ -80,7 +80,7 @@ int main() {
     cout << CheckSum() << endl << RunNumber() << endl;
     cout << "Theoretical: M = " << 3*(N - 1) << " C = " << (N*N - N)/2;
 
-    cout << "\nInc massive\n";
+    cout << "\n\nINCREASING MASSIVE\n";
     FillInc();
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
@@ -89,7 +89,7 @@ int main() {
     cout << CheckSum() << endl << RunNumber() << endl;
     cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
 
-    cout << "\nDec massive\n";
+    cout << "\n\nDECREASING MASSIVE\n";
     FillDec();
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
@@ -98,4 +98,3 @@ int main() {
     cout << CheckSum() << endl << RunNumber() << endl;
     cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
 }
-

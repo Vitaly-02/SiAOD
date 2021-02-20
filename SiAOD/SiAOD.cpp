0912319747087198fcad6b333c3e0409181a33a8
Vitaly Suslov <arthas.menethil2002@bk.ru>
2021-02-20 11:00:38 +0700

    3 лаба

--- SiAOD/SiAOD.cpp
+++ SiAOD/SiAOD.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 using namespace std;
 
-const int N = 20;
+const int N = 10;
 int A[N];
 
 void FillInc() {
@@ -50,51 +50,63 @@ int RunNumber() {
     return count;
 };
 
-void SelectSort(int Massive[], int size) {
+void SelectSort(int Massive[], int size) {  //upgrade commented
     int Moves = 0, Compares = 0;
-    for (int i = 0; i < N-1; i++) {
-        int min = i; 
-        if (RunNumber() != 1) {
-            for (int j = i + 1; j < N; j++) {
+    for (int i = 0; i < size-1; i++) {
+        int min = i;        
+            for (int j = i + 1; j < size; j++) {
                 Compares++;
                 if (Massive[j] < Massive[min]) {
                     min = j;
                 }
             }
-            if ((i != min) && (min > i)) {
+//            if ((i != min) && (min > i)) {
                 swap(Massive[i], Massive[min]);
+                Moves += 3;
+//            }
+    }
+    cout << "\nM = " << Moves << " C = " << Compares << endl;
+};
+
+void BubbleSort(int Massive[], int size) { 
+    int Moves = 0, Compares = 0;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = size-1; j > i ; j--) {
+            Compares++;
+            if (Massive[j] < Massive[j - 1]) {
+                swap(Massive[j], Massive[j - 1]);
                 Moves += 3;
             }
         }
     }
     cout << "\nM = " << Moves << " C = " << Compares << endl;
-};
+}
 
 int main() {
     cout << "RANDOM MASSIVE\n";
     FillRand();
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
-    SelectSort(A, N);
+    BubbleSort(A, N);
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
-    cout << "Theoretical: M = " << 3*(N - 1) << " C = " << (N*N - N)/2;
+    cout << "Theoretical: M = " << (3*((N*N - N)/2))/2 << " C = " << (N*N - N)/2;
 
     cout << "\n\nINCREASING MASSIVE\n";
     FillInc();
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
-    SelectSort(A, N);
+    BubbleSort(A, N);
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
-    cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
+    cout << "Theoretical: M = " << 0 << " C = " << (N*N - N)/2;
 
     cout << "\n\nDECREASING MASSIVE\n";
     FillDec();
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
-    SelectSort(A, N);
+    BubbleSort(A, N);
     PrintMas();
     cout << CheckSum() << endl << RunNumber() << endl;
-    cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
+    cout << "Theoretical: M = " << 3*((N*N - N)/2) << " C = " << (N*N - N)/2;
 }
---
+++ SiAOD/Source.cpp
@@ -0,0 +1,31 @@
+//main for SelectSort
+/*
+int main() {
+    cout << "RANDOM MASSIVE\n";
+    FillRand();
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    SelectSort(A, N);
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
+
+    cout << "\n\nINCREASING MASSIVE\n";
+    FillInc();
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    SelectSort(A, N);
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
+
+    cout << "\n\nDECREASING MASSIVE\n";
+    FillDec();
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    SelectSort(A, N);
+    PrintMas();
+    cout << CheckSum() << endl << RunNumber() << endl;
+    cout << "Theoretical: M = " << 3 * (N - 1) << " C = " << (N * N - N) / 2;
+}
+*/
